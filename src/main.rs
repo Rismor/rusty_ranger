023be0341f1cf3334917928c8a32877de06c4e7c
Morris Alromhein <morris4028@gmail.com>
2022-03-08 15:05:17 -0500

    Add Stateful List Struct for more TUI control

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,53 @@ struct Args {
     show_hidden: bool,
 }
 
+struct StatefulList<T> {
+    state: ListState,
+    items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                // Wrap to beginning of list
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                // Wrap to end of list
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn unselect(&mut self) {
+        self.state.select(None);
+    }
+}
 fn main() {
     println!("Hello, world!");
 }
